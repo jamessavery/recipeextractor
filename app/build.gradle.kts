2e6d5f62e5c1435771c6617b8a406211ca4a24e5
James Savery <83395912+jamessavery@users.noreply.github.com>
2024-08-06 17:57:03 +0100

    Migrated/refactor to Jetpack Compose (#5)
    - Refactored to Compose,  maintaining most of old design
    - Added & impl themes
    - Added TODO to fix broken button, as recipe history feature will replace it anyways
    
    ---------
    
    Co-authored-by: James Savery <{ID}+{username}@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,11 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 
     buildTypes {
@@ -40,7 +45,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -50,4 +54,13 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
 }
